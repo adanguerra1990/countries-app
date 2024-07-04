735808dfba5d530f18e09590fb925f618161c6d7
adanguerra1990 <aj90guerra@gmail.com>
2024-07-04 12:02:55 -0400

    custom styles for tailwindcss

---
+++ customStyles.js
@@ -0,0 +1,26 @@
+const customStyles = {
+     colors: {
+          neutral: {
+               darkBlue: 'hsl(209, 23%, 22%)', // Dark Mode Elements
+               veryDarkBlueBg: 'hsl(207, 26%, 17%)', // Dark Mode Background
+               veryDarkBlueText: 'hsl(200, 15%, 8%)', // Light Mode Text
+               darkGray: 'hsl(0, 0%, 52%)', // Light Mode Input
+               veryLightGray: 'hsl(0, 0%, 98%)', // Light Mode Background
+               white: 'hsl(0, 0%, 100%)', // Dark Mode Text & Light Mode Elements
+          },
+     },
+     fontSize: {
+          homePageItems: '14px',
+          detailPage: '16px',
+     },
+     fontFamily: {
+          sans: ['Nunito Sans', 'sans-serif'], // Define la fuente Nunito Sans
+     },
+     fontWeight: {
+          light: 300,
+          semiBold: 600,
+          bold: 800,
+     },
+}
+
+export default customStyles
