abadb4198ee1d2e45419a33274b51425b474f678
adanguerra1990 <aj90guerra@gmail.com>
2024-07-05 11:22:10 -0400

    Deploying container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,14 @@ import { RiMoonLine } from "@remixicon/react"
 
 const Header = () => {
   return (
-    <header className='p-6  bg-neutral-white shadow-md flex justify-between items-center'>
-      <h1 className="md:text-2xl font-bold">Where in the world</h1>
-      <button className="flex items-center space-x-2">
-        <RiMoonLine/>
-        <span>Dark Mode</span>
-      </button>
+    <header className='p-6  bg-neutral-white shadow-md '>
+      <div className="flex justify-between items-center container m-auto">
+        <h1 className="md:text-2xl font-bold">Where in the world</h1>
+        <button className="flex items-center space-x-2">
+          <RiMoonLine className="w-6 h-6"/>
+          <span>Dark Mode</span>
+        </button>
+      </div>
     </header>
   )
 }
