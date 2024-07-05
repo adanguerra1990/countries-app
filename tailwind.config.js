58f9ffc607aeb6491ddc64e44ea44bde1c2f5d55
adanguerra1990 <aj90guerra@gmail.com>
2024-07-05 09:22:38 -0400

    Setting custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+// tailwind.config.js
+const customStyles = require('./customStyles')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 
   content: [
+    "index.html",
     "./src/**/*.{js,jsx,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: customStyles.colors,
+      fontSize: customStyles.fontSize,
+      fontFamily: customStyles.fontFamily,
+      fontWeight: customStyles.fontWeight
+    },
   },
   plugins: [],
 }
