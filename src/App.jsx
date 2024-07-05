f4d7d96bce9f5684773108f8e8b926e7cd8a1f09
adanguerra1990 <aj90guerra@gmail.com>
2024-07-05 09:20:28 -0400

    Componet Header

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='h-screen flex justify-center items-center'>
+    <div>
       <Header />
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import { RiMoonLine } from "@remixicon/react"
 
 const Header = () => {
   return (
-    <div className='flex justify-center items-center'>
-      <h1 className='text-[25px]'>Hola</h1>
-    </div>
+    <header className='p-6  bg-neutral-white shadow-md flex justify-between items-center'>
+      <h1 className="md:text-2xl font-bold">Where in the world</h1>
+      <button className="flex items-center space-x-2">
+        <RiMoonLine/>
+        <span>Dark Mode</span>
+      </button>
+    </header>
   )
 }
 
