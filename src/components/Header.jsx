df110e6058441b49523b3c1ca35f0b9dda750adf
adanguerra1990 <aj90guerra@gmail.com>
2024-07-06 16:12:23 -0400

    style correction

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,12 @@ import { RiMoonLine } from "@remixicon/react"
 
 const Header = () => {
   return (
-    <header className='p-6  bg-neutral-white shadow-md '>
-      <div className="flex justify-between items-center container m-auto">
+    <header className='flex justify-between items-center px-4 sm:px-6 md:px-8 lg:px-12 mx-auto py-6 bg-neutral-white shadow-md '>      
         <h1 className="md:text-2xl font-bold">Where in the world</h1>
         <button className="flex items-center space-x-2">
           <RiMoonLine className="w-6 h-6"/>
           <span>Dark Mode</span>
-        </button>
-      </div>
+        </button>      
     </header>
   )
 }
