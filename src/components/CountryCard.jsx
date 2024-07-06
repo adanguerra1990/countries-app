e74f8414bb611be8a91b8144c083d062973276b7
adanguerra1990 <aj90guerra@gmail.com>
2024-07-06 16:11:27 -0400

    modifying components to request data from the API

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,18 +1,18 @@
-const CountryCard = ({ flag, name, population, region, capital }) => {
+const CountryCard = ({ country }) => {
      return (
           <article className="bg-neutral-white rounded  shadow-md overflow-hidden">
                <img
-                    src={flag}
-                    alt={`Flag of ${name}`}
+                    src={country.flags.png}
+                    alt={`${country.name.common} flag`}
                     className="w-full h-40 object-cover"
                />
                <div className="p-4">
                     <h2 className="text-xl font-bold mb-2">
-                         {name}
+                         {country.name.common}
                     </h2>
-                    <p><strong>Population:</strong> {population}</p>
-                    <p><strong>Region:</strong> {region}</p>
-                    <p><strong>Capital:</strong> {capital}</p>
+                    <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
+                    <p><strong>Region:</strong> {country.region}</p>
+                    <p><strong>Capital:</strong> {country.capital}</p>
                </div>
           </article>
      );
--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -1,25 +1,34 @@
 import { RiArrowDownSLine, RiSearchLine } from "@remixicon/react";
 import { useState } from "react";
 
-const SearchFilter = () => {
+const SearchFilter = ({ onSearch, onFilter }) => {
      const [filterOpen, setFilterOpen] = useState(false)
+     const [searchTerm, setSearchTerm] = useState('')
 
      const toggleFilter = () => {
           setFilterOpen(!filterOpen)
      }
 
-     const handleItemClick = () => {
+     const handleSearchChange = event => {
+          setSearchTerm(event.target.value)
+          onSearch(event.target.value)
+     }
+
+     const handleFilterClick = (region) => {
+          onFilter(region)
           setFilterOpen(false)
      }
      return (
-          <div className="flex justify-between items-center mb-6">
+          <div className="flex flex-col md:flex-row md:justify-between lg:items-center mb-6 lg:mb-6 space-y-4 lg:space-y-0 lg:space-x-4">
                <div className="relative w-full max-w-xs flex items-center justify-center">
                     <input
                          type="text"
                          placeholder="Search for a country..."
+                         value={searchTerm}
+                         onChange={handleSearchChange}
                          className="w-full p-2 pl-10 bg-neutral-white boreder rounded shadow-sm focus:outline-none"
                     />
-                    <RiSearchLine                         
+                    <RiSearchLine
                          className="absolute left-4 w-4 h-4 text-neutral-darkGray"
                     />
                </div>
@@ -34,11 +43,11 @@ const SearchFilter = () => {
                     </button>
                     {filterOpen && (
                          <ul className="absolute right-0 mt-2 w-48 bg-neutral-white border rounded shadow-md">
-                              <li className="p-2 hover:bg-neutral-darkGray cursor-pointer" onClick={() => handleItemClick('Africa')}>Africa</li>
-                              <li className="p-2 hover:bg-neutral-darkGray cursor-pointer" onClick={() => handleItemClick('America')}>America</li>
-                              <li className="p-2 hover:bg-neutral-darkGray cursor-pointer" onClick={() => handleItemClick('Asia')}>Asia</li>
-                              <li className="p-2 hover:bg-neutral-darkGray cursor-pointer" onClick={() => handleItemClick('Europe')}>Europe</li>
-                              <li className="p-2 hover:bg-neutral-darkGray cursor-pointer" onClick={() => handleItemClick('Oceania')}>Oceania</li>
+                              <li className="p-2 hover:bg-neutral-darkGray cursor-pointer" onClick={() => handleFilterClick('Africa')}>Africa</li>
+                              <li className="p-2 hover:bg-neutral-darkGray cursor-pointer" onClick={() => handleFilterClick('Americas')}>America</li>
+                              <li className="p-2 hover:bg-neutral-darkGray cursor-pointer" onClick={() => handleFilterClick('Asia')}>Asia</li>
+                              <li className="p-2 hover:bg-neutral-darkGray cursor-pointer" onClick={() => handleFilterClick('Europe')}>Europe</li>
+                              <li className="p-2 hover:bg-neutral-darkGray cursor-pointer" onClick={() => handleFilterClick('Oceania')}>Oceania</li>
                          </ul>
                     )}
                </div>
