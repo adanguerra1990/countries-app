3b76ac9a361af635b4f7a0002ade746c5720d29a
adanguerra1990 <aj90guerra@gmail.com>
2024-07-09 12:59:36 -0400

    Use the hook to get the current theme and the function to toggle it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
-import { RiMoonLine } from "@remixicon/react"
+import { RiMoonLine, RiSunLine } from "@remixicon/react"
+import useTheme from "../context/useTheme"
 
 const Header = () => {
+  const { theme, toggleTheme } = useTheme() // Usa el hook para obtener el tema actual y la función para alternarlo.
+  console.log('theme', theme)
+
   return (
-    <header className='flex justify-between items-center px-4 sm:px-6 md:px-8 lg:px-12 mx-auto py-6 bg-neutral-white shadow-md '>      
-        <h1 className="md:text-2xl font-bold">Where in the world</h1>
-        <button className="flex items-center space-x-2">
-          <RiMoonLine className="w-6 h-6"/>
-          <span>Dark Mode</span>
-        </button>      
+    <header className='flex justify-between items-center px-4 sm:px-6 md:px-8 lg:px-12 mx-auto py-6 bg-neutral-white dark:bg-neutral-darkBlue shadow-md '>
+      <h1 className="md:text-2xl font-bold">Where in the world</h1>
+      <button
+        onClick={toggleTheme} //Llama a toggleTheme cuando se hace clic en el botón
+        className="flex items-center space-x-2"
+      >
+        {theme === 'dark' ? <RiSunLine className="w-6 h-6" /> : <RiMoonLine className="w-6 h-6" />}
+        <span>{theme === 'dark' ? 'Light Mode' : 'Dark Mode'}</span>
+      </button>
     </header>
   )
 }
