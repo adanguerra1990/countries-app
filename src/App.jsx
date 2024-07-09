956ef31c5e82ecf8f74c0511de819f08a6fd41be
adanguerra1990 <aj90guerra@gmail.com>
2024-07-09 09:16:54 -0400

    Deploying  ShowMoreButtom

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import './index.css'
 import SearchFilter from './components/SearchFilter';
 import CountryCard from './components/CountryCard';
 import axios from 'axios';
+import ShowMoreButtom from './components/ShowMoreButtom';
 
 function App() {
   const [countries, setCountries] = useState([])
   const [filteredCountries, setFilteredCountries] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
   const [region, setRegion] = useState('')
-  console.log('region..', region)
+  const [visibleCount, setVisibleCount] = useState(8)  
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all')
       .then(respose => {
         setCountries(respose.data)
+        setFilteredCountries(respose.data)
       })
       .catch(error => {
         console.log('error feetching countries:', error)
@@ -40,6 +42,10 @@ function App() {
     setFilteredCountries(filtered)
   }, [searchTerm, region, countries]);
 
+  const handleShowMore = () => {
+    setVisibleCount(prevCount => prevCount + 8)
+  }
+
   return (
     <div>
       <Header />
@@ -49,13 +55,17 @@ function App() {
           onFilter={setRegion}
         />
         <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-          {filteredCountries.map(country => (
+          {filteredCountries.slice(0, visibleCount).map(country => (
             <CountryCard
               key={country.cca3}
               country={country}
             />
           ))}
         </section>
+        <ShowMoreButtom 
+          onClick={handleShowMore}
+          isVisible={visibleCount < countries.length}
+        />
       </main>
     </div>
   )
