d7d1523bd18b171634f5ac2de8e27d7af761c69c
adanguerra1990 <aj90guerra@gmail.com>
2024-07-22 11:42:03 -0400

    Implementing React Router to show country details

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,24 @@
-import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import './index.css'
-import SearchFilter from './components/SearchFilter';
-import CountryCard from './components/CountryCard';
-import axios from 'axios';
-import ShowMoreButtom from './components/ShowMoreButtom';
 import ThemeProvider from './components/ThemeProvider';
+import CountryDetails from './components/CountryDetails'
+import Home from './components/Home';
 
 function App() {
-  const [countries, setCountries] = useState([])
-  const [filteredCountries, setFilteredCountries] = useState([])
-  const [searchTerm, setSearchTerm] = useState('')
-  const [region, setRegion] = useState('')
-  const [visibleCount, setVisibleCount] = useState(8)
-
-  useEffect(() => {
-    axios.get('https://restcountries.com/v3.1/all')
-      .then(respose => {
-        setCountries(respose.data)
-        setFilteredCountries(respose.data)
-      })
-      .catch(error => {
-        console.log('error feetching countries:', error)
-      })
-  }, [])
-
-  useEffect(() => {
-    let filtered = countries
-
-    if (searchTerm) {
-      filtered = filtered.filter(country =>
-        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    }
-
-    if (region) {
-      filtered = filtered.filter(country =>
-        country.region === region
-      )
-    }
-
-    setFilteredCountries(filtered)
-  }, [searchTerm, region, countries]);
-
-  const handleShowMore = () => {
-    setVisibleCount(prevCount => prevCount + 8)
-  }
-
   return (
     <ThemeProvider>
-      <div>
-        <Header />
-        <main className='px-4 sm:px-6 md:px-8 lg:px-12 mx-auto py-6'>
-          <SearchFilter
-            onSearch={setSearchTerm}
-            onFilter={setRegion}
-          />
-          <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-            {filteredCountries.slice(0, visibleCount).map(country => (
-              <CountryCard
-                key={country.cca3}
-                country={country}
-              />
-            ))}
-          </section>
-          <ShowMoreButtom
-            onClick={handleShowMore}
-            isVisible={visibleCount < countries.length}
-          />
-        </main>
-      </div>
+      <Router>
+        <div>
+          <Header />
+          <main className='px-4 sm:px-6 md:px-8 lg:px-12 mx-auto py-6'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/country/:countryCode' Component={CountryDetails} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
     </ThemeProvider>
   )
 }
