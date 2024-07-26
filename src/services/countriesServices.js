ef3e65d1a6943f52b9f917bd0571fa74f4928985
adanguerra1990 <aj90guerra@gmail.com>
2024-07-26 11:37:40 -0400

    service file to organize API requests

---
+++ src/services/countriesServices.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+const baseUrl = 'https://restcountries.com/v3.1'
+
+const getAll = () => {
+     const request = axios.get(`${baseUrl}/all`)
+     return request.then(response => response.data)
+}
+
+const getByCode = (countryCode) => {
+     const request = axios.get(`${baseUrl}/alpha/${countryCode}`)
+     console.log(request)
+     return request.then(response => response.data)
+}
+
+export default {
+     getAll,
+     getByCode
+}
